795e2d2dbb4fb943d842c9118db8d122401d6e20
jackNEss <jackness1208@qq.com>
2019-05-10 18:54:28 +0800

    test finished

--- test/jest/test.js
+++ test/jest/test.js
@@ -177,27 +177,58 @@ if (TEST_CTRL.OPTIMIZE) {
     const configPath = path.join(FRAG_PATH, 'yyl.config.js');
     const iEnv = {};
     const config = await yh.parseConfig(configPath, iEnv);
-    yh.optimize.init({ config, iEnv });
-    await yh.optimize.afterTask(false);
+    const revPath = path.join(config.alias.revDest, yh.optimize.rev.filename);
+
+    const serverPluginPath = path.join(yh.vars.SERVER_PLUGIN_PATH, config.workflow);
+    await extFs.removeFiles(serverPluginPath);
 
     // 检查结果
-    // resource check
-    expect(fs.existsSync(config.alias.root, 'svga', 'logo.png')).toEqual(true);
+    const check = function() {
+      // plugins check
+      expect(fs.existsSync(path.join(serverPluginPath, 'node_modules/yyl-flexlayout'))).toEqual(true);
 
-    // concat check
-    expect(fs.existsSync(config.alias.jsDest, 'vendors.js')).toEqual(true);
+      // resource check
+      expect(fs.existsSync(path.join(config.alias.root, 'svga', 'logo.png'))).toEqual(true);
 
-    // rev check
-    expect(fs.existsSync(config.alias.revDest, yh.optimize.rev.filename)).toEqual(true);
+      // concat check
+      expect(fs.existsSync(path.join(config.alias.jsDest, 'vendors.js'))).toEqual(true);
 
-    // varSugar check
-    const htmlPath = path.join(config.alias.htmlDest, 'index.html');
-    const htmlCnt = fs.readFileSync(htmlPath).toString();
+      // rev check
+      expect(fs.existsSync(revPath)).toEqual(true);
 
-    const SUGAR_REG = /(\{\$)([a-zA-Z0-9@_\-$.~]+)(\})/g;
-    const URL_REG = /__url\(/g;
-    expect(htmlCnt.match(SUGAR_REG)).toEqual(null);
-    expect(htmlCnt.match(URL_REG)).toEqual(null);
+      // varSugar check
+      const htmlPath = path.join(config.alias.htmlDest, 'index.html');
+      const htmlCnt = fs.readFileSync(htmlPath).toString();
+
+      const SUGAR_REG = /(\{\$)([a-zA-Z0-9@_\-$.~]+)(\})/g;
+      const URL_REG = /__url\(/g;
+      expect(htmlCnt.match(SUGAR_REG)).toEqual(null);
+      expect(htmlCnt.match(URL_REG)).toEqual(null);
+    };
+
+    yh.optimize.init({ config, iEnv });
+
+    // 构建模拟
+    await yh.optimize.initPlugins();
+    await yh.optimize.afterTask(false);
+    check();
+    const revData1 = JSON.parse(fs.readFileSync(revPath).toString());
+
+    // 更新模拟
+    await extFs.copyFiles(casePath, FRAG_PATH);
+
+    // 文件变更模拟
+    const vendorsPath = path.join(config.alias.jsDest, 'vendors.js');
+    let vCnt = fs.readFileSync(vendorsPath).toString();
+    vCnt = `console.log(1123);${vCnt}`;
+    fs.writeFileSync(vendorsPath, vCnt);
+
+    await yh.optimize.afterTask(true);
+    check();
+    const revData2 = JSON.parse(fs.readFileSync(revPath).toString());
+
+    // rev hash 变更校验
+    expect(revData1).toEqual(revData2);
 
     // 销毁
     await fn.frag.destroy();
