fa8134ebc65e52c38c36471480fefe4354e68d5c
liudaojie <liudaojie@yy.com>
2020-02-18 23:23:37 +0800

    feat: 用例编写完成

--- test/jest/html2string.test.js
+++ test/jest/html2string.test.js
@@ -2,7 +2,7 @@ const { Handler, log, vars } = require('../lib/const');
 
 const yh = new Handler( { log, vars });
 
-test('yh.html2string(iPath): string', () => {
+test('yh.html2string(...class="abc"...): string', () => {
   expect(yh.html2string(
     `
       <div class="abc"></div>
@@ -11,5 +11,98 @@ test('yh.html2string(iPath): string', () => {
   )).toEqual(`
     <div class=\\"abc\\"></div>
   `.trim())
+  ;
+
+  expect(yh.html2string(
+    `
+      <div class="abc"></div>
+    `,
+    '\''
+  )).toEqual(`
+    <div class="abc"></div>
+  `.trim())
+  ;
+});
+
+test('yh.html2string(...class=\'abc\'...): string', () => {
+  expect(yh.html2string(
+    `
+      <div class='abc'></div>
+    `,
+    '\''
+  )).toEqual(`
+    <div class=\\'abc\\'></div>
+  `.trim())
+  ;
+
+  expect(yh.html2string(
+    `
+      <div class='abc'></div>
+    `,
+    '"'
+  )).toEqual(`
+    <div class='abc'></div>
+  `.trim())
+  ;
+});
+
+test('yh.html2string(...title=\'1"2"3\'...): string', () => {
+  expect(yh.html2string(
+    `
+      <div title='1"2"3'></div>
+    `,
+    '\''
+  )).toEqual(`
+      <div title=\\'1"2"3\\'></div>
+  `.trim())
+  ;
+
+  expect(yh.html2string(
+    `
+      <div title='1"2"3'></div>
+    `,
+    '"'
+  )).toEqual(`
+      <div title='1\\"2\\"3'></div>
+  `.trim())
+  ;
+});
+
+test('yh.html2string(...title=\'1\\\'2\\\'3\'...): string', () => {
+  expect(yh.html2string(
+    `
+      <div title='1\\'2\\'3'></div>
+    `,
+    '\''
+  )).toEqual('<div title=\\\'1\\\\\'2\\\\\'3\\\'></div>'.trim());
+
+  expect(yh.html2string(
+    `
+      <div title=\\'1'2'3\\'></div>
+    `,
+    '"'
+  )).toEqual(`
+      <div title=\\'1'2'3\\'></div>
+  `.trim())
+  ;
+});
+
+test('yh.html2string(...title="1\\"2\\"3"...): string', () => {
+  expect(yh.html2string(
+    `
+      <div title="1\\"2\\"3"></div>
+    `,
+    '\''
+  )).toEqual(`
+      <div title="1\\"2\\"3"></div>
+  `.trim())
+  ;
+
+  expect(yh.html2string(
+    `
+      <div title="1\\"2\\"3"></div>
+    `,
+    '"'
+  )).toEqual('<div title=\\"1\\\\"2\\\\"3\\"></div>')
   ;
 });
