db2ccb600a2bd25731b27fb9c45fe441b5c436ba
liudaojie <liudaojie@yy.com>
2021-02-09 18:24:27 +0800

    feat: rebuild

--- README.md
+++
@@ -1,4 +0,0 @@
-# yy-hander
-
-## api
-[./lib/yh.d.ts](./lib/yh.d.ts)
--- test/jest/setVars.test.js
+++
@@ -1,9 +0,0 @@
-const { Handler, log, vars } = require('../lib/const')
-
-const yh = new Handler({ log, vars })
-
-test('yh.setVars(vars): IVars', () => {
-  const r = yh.setVars({ PROJECT_PATH: __dirname })
-  expect(r.PROJECT_PATH).toEqual(__dirname)
-  expect(yh.vars.PROJECT_PATH).toEqual(__dirname)
-})
